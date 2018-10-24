40c8e420707cbab4dc95355b9b96db88e745ec80
zhou-ting <zt@hklh8.com>
2018-10-24 23:15:46 +0800

    Update index.js

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -3,13 +3,14 @@ import { WebView, View, StyleSheet, Platform } from 'react-native';
 import renderChart from './renderChart';
 import echarts from './echarts.min';
 
+const source = Platform.OS === 'ios' ? require('./tpl.html') : { uri: 'file:///android_asset/echarts.html' };
+
 export default class App extends Component {
 
   constructor(props) {
     super(props);
     this.setNewOption = this.setNewOption.bind(this);
   }
-  
 
   componentWillReceiveProps(nextProps) {
     if(nextProps.option !== this.props.option) {
@@ -33,7 +34,7 @@ export default class App extends Component {
             backgroundColor: this.props.backgroundColor || 'transparent'
           }}
           scalesPageToFit={Platform.OS !== 'ios'}
-          source={require('./tpl.html')}
+          source={source}
           onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
         />
       </View>
